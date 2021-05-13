a972e821d2394c0839566aa3c5880bb602bf8620
eun <soc06241@naver.com>
2021-05-13 22:48:10 +0900

    first commit

---
+++ example1.js
@@ -0,0 +1,10 @@
+//변수 선언 방법 1
+let espressoPrice;
+espressoPrice = 3000;
+
+//변수 선언 방법 2
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+//출력
+console.log(espressoPrice * 3 + lattePrice * 2 + mochaPrice);
